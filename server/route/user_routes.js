7cc8595dc0d3bc823843db21ca1775e9e3790b15
MenchenFive <amenchenr@gmail.com>
2019-04-04 22:06:12 +0200

    now users are retreived with their roles

--- server/route/user_routes.js
+++ server/route/user_routes.js
@@ -4,10 +4,29 @@ const router = express.Router();
 const user = require('../model/users.js');
 const role = require('../model/user_roles.js');
 
+const addRoleToUser = async (data) => {
+    return Promise.all(
+        data.map(
+            async element => {
+                element.dataValues.role = await role.findAll( { where: { id:element.dataValues.refRole } } );
+            }
+        )
+    );
+}
+
 router.get('/', async (req,res) => {
     try {
-        res.json( await user.findAll() );
-        res.json( "patata" );
+
+        let result = await user.findAll()//.map( (r) => (r.toJSON()));
+
+        /*for (let u of result) {
+            u.a = await role.findAll({where:{id:u.refRole}});
+        }*/
+
+        await addRoleToUser (result);
+
+        res.json( result );
+
     } catch (error) {
         console.log(error);
     }
@@ -20,5 +39,7 @@ router.post('/', dao.insert);
 router.put('/:id', dao.edit);
 
 router.delete('/:id', dao.delete);*/
+
+
 
 module.exports = router;
